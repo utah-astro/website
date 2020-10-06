a278e0322733998a03902c2e912b91d9076b47e7
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-06 17:29:41 -0600

    test

---
+++ html/head.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="en" class="no-js">
+<!--<![endif]-->
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0">
+    <meta charset="utf-8">
+    <title>Astronomy &amp; Astrophysics  - Department of Physics &amp; Astronomy - The University of
+             Utah
+          </title>
+    <meta name="keywords" content="physics,astronomy,university,'university of utah',astrophysics,atomic,'condensed matter',dark,everything,experimental,'high energy','cosmic ray',nanoscience,education,'higher education','nanoscience is huge',nanoscale,neutrinos,'newtonian motion',einstein,feynman,research,'whats that in the sky',colloquium,'solid state',science,'things in space',aliens,'space the final frontier',zoom,theoretical,experimental,spectrum,stars,galaxy,light,nurse,doctor,'life the universe everything','scanning electron microscopy',utes,observatory,telescope,'frisco peak', satellite,moon,milkyway,'milky way',novas,dwarfs,planets,universe,cosmology,'observational astronomy',biophysics,relativity,particles,light,'particle theory',acoustics,'heat acoustics','materials science',ufos,muons,darkmatter, 'dark matter',neutrino,'neutrino detector','graduate school','award winning'">
+    <meta name="description" content="Come see the exciting opportunities in research and education that we have to offer at the Department of Physics &amp; Astronomy at the University of Utah.">
+    <meta name="breadcrumb" content="Overview">
+    <meta name="robots" content="index,follow">
+    <link rel="shortcut icon" href="https://templates.utah.edu/_main-v2/_images/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon-precomposed" href="https://templates.utah.edu/_main-v2/_images/apple-touch-icon.png">
+    <link href="https://templates.utah.edu/_main-v2/_css/main.css" rel="stylesheet" type="text/css" media="all">
+    <link href="../css/custom-styles.css" rel="stylesheet" type="text/css" media="all">
+
+    <!--[if lt IE 9]>
+          <script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+          <link href="https://templates.utah.edu/_main-v2/respond-proxy.html" id="respond-proxy" rel="respond-proxy">
+
+          <link href="https://templates.utah.edu/_images/respond.proxy.gif" id="respond-redirect" rel="respond-redirect">
+          <script src="/_scripts/respond.proxy.js"></script>
+          <![endif]-->
+
+    <script src="//templates.utah.edu/_main-v2/_scripts/vendor/modernizr-2.6.2.min.js"></script>
+    <!-- Code to be included before the closing </head> tag can go here -->
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139456402-3"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-139456402-3');
+    </script>
+
+</head>
