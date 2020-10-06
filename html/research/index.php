39fcc0efd5c3ed1a8352fba85a1f23087253d57f
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-06 17:48:31 -0600

    research

--- html/research/index.php
+++ html/research/index.php
@@ -32,81 +32,79 @@
                 <div class="uu-main-bottom row">
                     <div class="uu-main-column">
                         <div class="uu-main-content">
-
-<ul>
-<li><b>Cosmology and the large-scale structure of the Universe.</b>
-We look at how galaxies in the universe are patterned to
-form clumps and clusters, connected by thin filaments, almost
-like a cosmic sponge. One mysterious entity, dark matter,
-helps form the clumps, while another, dark energy, is starting
-to drive an accelerated expansion of these clumps away from each
-other. A main goal is to identify both dark matter and dark energy.
-[<a href="http://www.physics.utah.edu/~joelbrownstein/">Brownstein</a>, <a href="http://www.physics.utah.edu/~kdawson/">Dawson</a>, <a href="http://www.physics.utah.edu/~wik/">Wik</a>, <a href="http://www.physics.utah.edu/~zhengzheng/">Zheng</a>]
-<br>&nbsp;
-<br>
-
-
-<li><b>Galaxy evolution and dynamics.</b>  Galaxies, including our own
-Milky Way, each contain billions of stars (give or take!), as well as
-mysterious dark matter. We look to see how galaxies were assembled and
-how they interact with each other, giving clues about the formation of
-stars and the nature of dark matter. [<a href="http://www.physics.utah.edu/~joelbrownstein/">Brownstein</a>, <a href="http://www.physics.utah.edu/~iii/">Ivans</a>, <a href="http://www.physics.utah.edu/~aseth/">Seth</a>, <a href="http://www.physics.utah.edu/~zasowski/">Zasowski</a>, <a href="http://www.physics.utah.edu/~zhengzheng/">Zheng</a>]
-<br>&nbsp;
-<br>
-
-<li><b>Galaxy nuclei and massive black holes.</b>
-Most, if not all, galaxies harbor supermassive black holes.
-These exotic objects have the mass of millions to billions of
-stars packed into a region of space smaller than our solar system.
-We look at how these objects grow and influence the growth of
-their host galaxies. Their origin remains a mystery....
-[<a href="http://www.physics.utah.edu/~aseth/">Seth</a>, <a href="http://www.physics.utah.edu/~bromley/">Bromley</a>, <a href="http://www.physics.utah.edu/~wik/">Wik</a>]
-<br>&nbsp;
-<br>
-
-<li><b>Stars in the Milky Way and the origins of elements.</b>
-Stellar spectroscopy, the detailed analysis of starlight
-in terms of its color, reveals the elements that make up stars.
-By studying the how these elements are spread across
-different populations of stars in the Milky Way Galaxy,
-we can understand the origin of carbon, oxygen and other
-elements of which we are composed. Spectroscopy also tells us about how the stars are moving, how old they are, and other properties that let us understand how the Milky Way formed and has changed over time.  [<a href="http://www.physics.utah.edu/~iii/">Ivans</a>, <a href="http://www.physics.utah.edu/~aseth/">Seth</a>, <a href="http://www.physics.utah.edu/~zasowski/">Zasowski</a>]
-<br>&nbsp;
-<br>
-
-<li><b>Dark matter.</b> Dark matter is known only
-from its gravitational influence on visible stars and gas.
-It's properties may be inferred from how it affects
-galaxy clustering. A major goal of astronomy is to
-detect dark matter directly by catching a rare
-collision with normal atoms. Alternatively dark matter
-may reveal itself in interactions that can lead to
-showers of high energy particles. So far there
-have been only tentative hints of detections. Thus,
-the mystery continues. [<a href="http://www.physics.utah.edu/~joelbrownstein/">Brownstein</a>, <a href="http://faculty.utah.edu/u0410334-PAOLO_GONDOLO/biography/index.hml">Gondolo</a>, <a href="http://www.physics.utah.edu/~sandick/">Sandick</a>, <a href="http://www.physics.utah.edu/~wik/">Wik</a>, <a href="http://www.physics.utah.edu/~zhaoyue/">Zhao</a>]
-<br>&nbsp;
-<br>
-
-<li><b>Experimental and Observational High Energy Astrophysics.</b> Utah is
-part of VERITAS, HAWC, and TA collaborations looking
-at high-energy particles in the cosmos. The
-source of these particles is not entirely certain,
-but some likely involve physics near the
-edge of supermassive black holes. [<a href="http://faculty.utah.edu/u0028378-DAVID_B_KIEDA/biography/index.hml">Kieda</a>, <a href="http://www.physics.utah.edu/~springer/">Springer</a>, <a href="http://www.physics.utah.edu/~wik/">Wik</a>, <a href="http://www.physics.utah.edu/index.php/research/cosmic-rays">Cosmic Ray Group</a>].
-<br>&nbsp;
-<br>
-
-<li><b>Planets.</b> Amidst dark energy and dark matter, within
-huge galaxies and around stars, small particles of dust began
-to glom together to form planets. Our view of planets in the
-Universe has expanded from just a handful around our solar system
-to thousands seen around other stars. We are working to understand
-the mechanisms at play in the formation planets, in particular
-rocky planets like our own Earth. Is this process universal and
-do we have company in the Universe? [<a href="http://www.physics.utah.edu/~bromley/">Bromley</a>]
+                            <ul>
+                            <li><b>Cosmology and the large-scale structure of the Universe.</b>
+                            We look at how galaxies in the universe are patterned to
+                            form clumps and clusters, connected by thin filaments, almost
+                            like a cosmic sponge. One mysterious entity, dark matter,
+                            helps form the clumps, while another, dark energy, is starting
+                            to drive an accelerated expansion of these clumps away from each
+                            other. A main goal is to identify both dark matter and dark energy.
+                            [<a href="http://www.physics.utah.edu/~joelbrownstein/">Brownstein</a>, <a href="http://www.physics.utah.edu/~kdawson/">Dawson</a>, <a href="http://www.physics.utah.edu/~wik/">Wik</a>, <a href="http://www.physics.utah.edu/~zhengzheng/">Zheng</a>]
+                            <br>&nbsp;
+                            <br>
 
 
+                            <li><b>Galaxy evolution and dynamics.</b>  Galaxies, including our own
+                            Milky Way, each contain billions of stars (give or take!), as well as
+                            mysterious dark matter. We look to see how galaxies were assembled and
+                            how they interact with each other, giving clues about the formation of
+                            stars and the nature of dark matter. [<a href="http://www.physics.utah.edu/~joelbrownstein/">Brownstein</a>, <a href="http://www.physics.utah.edu/~iii/">Ivans</a>, <a href="http://www.physics.utah.edu/~aseth/">Seth</a>, <a href="http://www.physics.utah.edu/~zasowski/">Zasowski</a>, <a href="http://www.physics.utah.edu/~zhengzheng/">Zheng</a>]
+                            <br>&nbsp;
+                            <br>
 
+                            <li><b>Galaxy nuclei and massive black holes.</b>
+                            Most, if not all, galaxies harbor supermassive black holes.
+                            These exotic objects have the mass of millions to billions of
+                            stars packed into a region of space smaller than our solar system.
+                            We look at how these objects grow and influence the growth of
+                            their host galaxies. Their origin remains a mystery....
+                            [<a href="http://www.physics.utah.edu/~aseth/">Seth</a>, <a href="http://www.physics.utah.edu/~bromley/">Bromley</a>, <a href="http://www.physics.utah.edu/~wik/">Wik</a>]
+                            <br>&nbsp;
+                            <br>
+
+                            <li><b>Stars in the Milky Way and the origins of elements.</b>
+                            Stellar spectroscopy, the detailed analysis of starlight
+                            in terms of its color, reveals the elements that make up stars.
+                            By studying the how these elements are spread across
+                            different populations of stars in the Milky Way Galaxy,
+                            we can understand the origin of carbon, oxygen and other
+                            elements of which we are composed. Spectroscopy also tells us about how the stars are moving, how old they are, and other properties that let us understand how the Milky Way formed and has changed over time.  [<a href="http://www.physics.utah.edu/~iii/">Ivans</a>, <a href="http://www.physics.utah.edu/~aseth/">Seth</a>, <a href="http://www.physics.utah.edu/~zasowski/">Zasowski</a>]
+                            <br>&nbsp;
+                            <br>
+
+                            <li><b>Dark matter.</b> Dark matter is known only
+                            from its gravitational influence on visible stars and gas.
+                            It's properties may be inferred from how it affects
+                            galaxy clustering. A major goal of astronomy is to
+                            detect dark matter directly by catching a rare
+                            collision with normal atoms. Alternatively dark matter
+                            may reveal itself in interactions that can lead to
+                            showers of high energy particles. So far there
+                            have been only tentative hints of detections. Thus,
+                            the mystery continues. [<a href="http://www.physics.utah.edu/~joelbrownstein/">Brownstein</a>, <a href="http://faculty.utah.edu/u0410334-PAOLO_GONDOLO/biography/index.hml">Gondolo</a>, <a href="http://www.physics.utah.edu/~sandick/">Sandick</a>, <a href="http://www.physics.utah.edu/~wik/">Wik</a>, <a href="http://www.physics.utah.edu/~zhaoyue/">Zhao</a>]
+                            <br>&nbsp;
+                            <br>
+
+                            <li><b>Experimental and Observational High Energy Astrophysics.</b> Utah is
+                            part of VERITAS, HAWC, and TA collaborations looking
+                            at high-energy particles in the cosmos. The
+                            source of these particles is not entirely certain,
+                            but some likely involve physics near the
+                            edge of supermassive black holes. [<a href="http://faculty.utah.edu/u0028378-DAVID_B_KIEDA/biography/index.hml">Kieda</a>, <a href="http://www.physics.utah.edu/~springer/">Springer</a>, <a href="http://www.physics.utah.edu/~wik/">Wik</a>, <a href="http://www.physics.utah.edu/index.php/research/cosmic-rays">Cosmic Ray Group</a>].
+                            <br>&nbsp;
+                            <br>
+
+                            <li><b>Planets.</b> Amidst dark energy and dark matter, within
+                            huge galaxies and around stars, small particles of dust began
+                            to glom together to form planets. Our view of planets in the
+                            Universe has expanded from just a handful around our solar system
+                            to thousands seen around other stars. We are working to understand
+                            the mechanisms at play in the formation planets, in particular
+                            rocky planets like our own Earth. Is this process universal and
+                            do we have company in the Universe? [<a href="http://www.physics.utah.edu/~bromley/">Bromley</a>]
+
+                            </ul>
                         </div>
                     </div>
                     <?php include '../submenu.php';?>
