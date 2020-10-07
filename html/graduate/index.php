7aa4c9462fb47d9f8ca3d1ca0e5eb026477792cf
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-06 18:04:02 -0600

    grad

---
+++ html/graduate/index.php
@@ -0,0 +1,60 @@
+<?php include '../head.php';?>
+<body class="two-column-left">
+    <!-- Code to be included after the opening <body> tag can go here -->
+    <div class="uu-page-wrapper">
+        <?php include '../header.php';?>
+        <div class="uu-breadcrumb">
+            <div class="uu-container">
+                <p class="sr-only">You are here:</p>
+                <ol class="breadcrumb">
+                    <li>
+                        <a href="https://www.physics.utah.edu/">Home</a>
+                    </li>
+                    <li>
+                        <a href="https://www.physics.utah.edu/research/">Research</a>
+                    </li>
+                    <li>
+                        <a href="http://www.physics.utah.edu/research/astronomy-astrophysics.php">Astronomy &amp; Astrophysics</a>
+                    </li>
+                    <li class="active">Graduate Program</li>
+                </ol>
+            </div>
+        </div>
+        <main class="uu-main">
+            <div class="uu-container">
+                <div class="uu-main-top row" id="uu-skip-target">
+                    <div class="uu-top-content">
+                        <h1 class="page-title">Astronomy and High Energy Astrophysics: Graduate Program</h1>
+                    </div>
+                </div>
+                <div class="uu-main-bottom row">
+                    <div class="uu-main-column">
+                        <div class="uu-main-content">
+                            <p><img class="img-float-left" src="/images/banner/bnr_gradprogram.jpg"></p>
+                            <p>
+                                The University of Utah now offers a full array of
+                                courses in graduate physics, astrophysics and astronomy. These
+                                courses, along with research opportunites with our faculty,
+                                help enable students to receive
+                                a Ph.D. in Physics with Emphasis in Astronomy and Astrophysics.
+
+                            </p>
+                            <p>
+                                This page provides some resources for you if you are currently
+                                a Ph.D. student or are interested in becoming one:
+                                <ul>
+                                    <li><a href="http://www.physics.utah.edu/index.php/graduate-program">Departmental Graduate information</a></li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                    <?php include '../submenu.php';?>
+                </div>
+            </div>
+        </main>
+        <?php include '../footer.php';?>
+    </div>
+    <?php include '../scripts.php';?>
+</body>
+</html>
+
