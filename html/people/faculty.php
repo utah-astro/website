b8de0773700ad6f868e13006cbf5c041485d5658
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-09 13:51:55 -0600

    test

--- html/people/faculty.php
+++ html/people/faculty.php
@@ -1,23 +1,30 @@
 <h2>Astro Group Faculty</h2>
+<table class="table table-header-shade table-bordered">
 <?php
-    $members = array(
-        "Ben Bromley" => array(
-            "image" => "https://faculty.utah.edu/bytes/image.hml?id=u0095165",
-            "homepage" => "http://www.physics.utah.edu/~bromley",
-            "keywords" => "Planets"),
-        "Joel Brownstein" => array(
-            "image" => "https://faculty.utah.edu/bytes/image.hml?id=u0707758",
-            "homepage" => "https://faculty.utah.edu/~joelbrownstein",
-            "keywords" => "Galaxies")
-    );
+    try {
+        $members = array(
+            "Ben Bromley" => array(
+                "image" => "https://faculty.utah.edu/bytes/image.hml?id=u0095165",
+                "homepage" => "http://www.physics.utah.edu/~bromley",
+                "keywords" => "Planets"),
+            "Joel Brownstein" => array(
+                "image" => "https://faculty.utah.edu/bytes/image.hml?id=u0707758",
+                "homepage" => "https://faculty.utah.edu/~joelbrownstein",
+                "keywords" => "Galaxies")
+        );
 
-    <table class="table table-header-shade table-bordered">
         foreach ($members as $key => $value) {
-            <tr>
-                <td>
-                    <span>$key</span>
-                </td>
-            </tr>
+            echo "<tr>";
+            echo "    <td width='150'><img src='$member->image' alt='$membername' width='150' height='150'></td>";
+            echo "    <td>";
+            echo "        <h4><a href='$member->homepage'>$membername</a></h4>";
+            echo "        <span>$member->keywords</span>";
+            echo "    </td>";
+            echo "</tr>";
         }
-    </table>
+        
+    } catch (Exception $e) {
+        echo 'Exception: ',  $e->getMessage(), "\n";
+    }
+</table>
 ?>
