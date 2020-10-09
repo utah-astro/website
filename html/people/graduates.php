d49bfdd82da50f66e9669347f7ff323e38310966
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-09 13:17:34 -0600

    test

--- html/people/graduates.php
+++ html/people/graduates.php
@@ -1,7 +1,7 @@
 <h2>Astro Group Graduate Students</h2>
 <table class="table table-header-shade table-bordered">
     <tr>
-        <td><img src="" alt="Grad-Name" width="100" height="100"></td>
+        <td width="150"><img src="https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg" alt="Grad-Name" width="150" height="150"></td>
         <td>
             <h4><a href="">Grad-Name</a></h4>
             <span>Grad-Keywords</span>
--- html/people/postdocs.php
+++ html/people/postdocs.php
@@ -1,7 +1,7 @@
 <h2>Astro Group Postdocs</h2>
 <table class="table table-header-shade table-bordered">
     <tr>
-        <td><img src="" alt="Postdoc-Name" width="100" height="100"></td>
+        <td width="150"><img src="https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg" alt="Postdoc-Name" width="150" height="150"></td>
         <td>
             <h4><a href="">Postdoc-Name</a></h4>
             <span>Postdoc-Keywords</span>
