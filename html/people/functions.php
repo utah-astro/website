5336844115d3e1134a04210f8044141e77874b78
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-09 15:47:28 -0600

    test

--- html/people/functions.php
+++ html/people/functions.php
@@ -1,6 +1,6 @@
 <?php
     function membertable($members) {
-        if (isset($members)) {
+        if ( isset($members) ) {
             echo "<table class='table table-header-shade table-bordered'>";
             foreach ($members as $name => $member) {
                 $image = $member["image"];
@@ -11,8 +11,8 @@
                 echo "    <td width='150'><a href='$homepage'><img src='$image' alt='$name' width='150' height='150'></a></td>";
                 echo "    <td>";
                 echo "        <h4><a href='$homepage'>$name</a></h4>";
-                //if (isset($keywords)) {echo "        <span>$keywords</span>";}
-                //if (isset($datascience)) {echo "        <span>$datascience</span>";}
+                if ( isset($keywords) ) {echo "        <span>$keywords</span>";}
+                if ( isset($datascience) ) {echo "        <span>$datascience</span>";}
                 echo "    </td>";
                 echo "</tr>";
             }
