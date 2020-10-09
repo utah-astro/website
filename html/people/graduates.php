120e1cf0ad21697585573540a2ef3d2d748f89fb
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-09 13:36:15 -0600

    test

--- html/people/graduates.php
+++ html/people/graduates.php
@@ -1,10 +1,10 @@
 <h2>Astro Group Graduate Students</h2>
 <table class="table table-header-shade table-bordered">
     <tr>
-        <td width="150"><img src="https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg" alt="Grad-Name" width="150" height="150"></td>
+        <td width="150"><img src="https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg" width="150" height="150"></td>
         <td>
-            <h4><a href="">Grad-Name</a></h4>
-            <span>Grad-Keywords</span>
+            <h4><a href="">Add name here</a></h4>
+            <span>Research topics</span>
         </td>
     </tr>
 </table>
--- html/people/postdocs.php
+++ html/people/postdocs.php
@@ -1,10 +1,10 @@
 <h2>Astro Group Postdocs</h2>
 <table class="table table-header-shade table-bordered">
     <tr>
-        <td width="150"><img src="https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg" alt="Postdoc-Name" width="150" height="150"></td>
+        <td width="150"><img src="https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg" width="150" height="150"></td>
         <td>
-            <h4><a href="">Postdoc-Name</a></h4>
-            <span>Postdoc-Keywords</span>
+            <h4><a href="">Add name here</a></h4>
+            <span>Research topics</span>
         </td>
     </tr>
 </table>
