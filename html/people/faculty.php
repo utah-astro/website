87c054852c2e61f33669ff501d38028908946519
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-09 13:54:53 -0600

    test

--- html/people/faculty.php
+++ html/people/faculty.php
@@ -12,11 +12,11 @@
                 "keywords" => "Galaxies")
         );
         echo "<table class='table table-header-shade table-bordered'>";
-        foreach ($members as $key => $value) {
+        foreach ($members as $name => $member) {
             echo "<tr>";
-            echo "    <td width='150'><img src='$member->image' alt='$membername' width='150' height='150'></td>";
+            echo "    <td width='150'><img src='$member->image' alt='$name' width='150' height='150'></td>";
             echo "    <td>";
-            echo "        <h4><a href='$member->homepage'>$membername</a></h4>";
+            echo "        <h4><a href='$member->homepage'>$name</a></h4>";
             echo "        <span>$member->keywords</span>";
             echo "    </td>";
             echo "</tr>";
