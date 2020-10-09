9f9468274e4e5b1ab108e100fa69c638ae9af7d1
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-09 11:47:52 -0600

    people

--- html/people/index.php
+++ html/people/index.php
@@ -219,156 +219,16 @@
                                     <h3 style="text-align: center;">&nbsp;</h3>
                                 </div>
                             </div>
-                            <h2>&nbsp;High Energy Astrophysics Faculty</h2>
+                            <h2>&nbsp;Astro Group Postdocs</h2>
                             <div class="row layout-grid ">
                                 <div class="grid-item col-4">
-                                    <a href="http://faculty.utah.edu/u0410334-PAOLO_GONDOLO/biography/index.hml">
-                                        <img src="https://www.physics.utah.edu/_images/professors_research/Abeysekara.png" alt="Udara Abeysekara" width="640" height="640">
+                                    <a href="">
+                                        <img src="" alt="Person" width="640" height="640">
                                     </a>
-                                    <h3 style="text-align: center;">Anushka Abeysekara</h3>
+                                    <h3 style="text-align: center;">Person</h3>
                                     <p>
-                                        Anushka is studying the most extreme environments in the Universe--such as gamma-ray
-                                                                         emitting supernova remnants, pulsar&nbsp;
-                                        <span>wind nebulae, and active galactic nuclei that accelerate particles to speeds near
-                                                                            the speed of light. These particles emit very-high-energy gamma-rays as they interact
-                                                                            with the surrounding medium. Using HAWC and VERITAS gamma-ray observatories, he measures
-                                                                            these gamma-rays and investigate questions such as, how these particles are moving
-                                                                            away from the central engine, what fraction of the particles reach earth, and what
-                                                                            is the nature of the medium between Earth and these objects.</span>
+                                        blurb....
                                     </p>
-                                    <ul>
-                                        <li>
-                                            <a href="/gammaray/">Gamma Ray Astronomy/VERITAS</a>
-                                        </li>
-                                        <li>
-                                            <a href="http://www.hawc-observatory.org/">HAWC</a>
-                                        </li>
-                                        <li>
-                                            <a href="/starbase/">StarBase</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="grid-item col-4">
-                                    <a href="https://faculty.utah.edu/u0786002-PEARL_SANDICK/biography/index.hml">
-                                        <img src="https://www.physics.utah.edu/_images/professors_research/Gondolo.png" alt="Paolo Gondolo" width="430" height="430">
-                                    </a>
-                                    <h3 style="text-align: center;">Paolo Gondolo</h3>
-                                    <p>Cosmology, astrophysics, high energy physics.</p>
-                                    <ul>
-                                        <li>
-                                            <a href="/astroinstitute/astroinstitute.html">High Energy Astrophysics Institute</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="grid-item col-4">
-                                    <a href="http://www.physics.utah.edu/~zhaoyue/">
-                                        <img src="https://www.physics.utah.edu/_images/professors_research/Kieda.png" alt="Dave Kieda" width="437" height="437">
-                                    </a>
-                                    <h3 style="text-align: center;">Dave Kieda</h3>
-                                    <p>
-                                        <span>Dave's research focuses on the exploration of the fundamental forces in physics through
-                                                                            the measurement of astrophysical objects in extreme settings. He develops techniques
-                                                                            and performs observations in high energy gamma-ray astronomy to explore particle acceleration
-                                                                            in Galactic objects, such as supernova remnants, microquasars and high mass x-ray
-                                                                            binaries, and active galactic nuclei. He also is leading an effort to develop a new
-                                                                            capability for ultra-high angular resolution (&lt; 40 microarc-second) optical imaging
-                                                                            of nearby stars using the quantum properties of light. His experimental observations
-                                                                            use the ground-based VERITAS, HAWC , CTA, and StarBase-Utah observatories as well
-                                                                            as the FERMI, Swift and NuStar satellite observatories.&nbsp;&nbsp;</span>
-                                    </p>
-                                    <ul>
-                                        <li>
-                                            <a href="/gammaray/">Gamma Ray Astronomy/VERITAS</a>
-                                        </li>
-                                        <li>
-                                            <a href="http://www.hawc-observatory.org/">HAWC</a>
-                                        </li>
-                                        <li>
-                                            <a href="/starbase/">StarBase</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="grid-item col-4">
-                                    <a href="https://faculty.utah.edu/u0786031-ZHENG_ZHENG/research/index.hml">
-                                        <img src="https://www.physics.utah.edu/_images/professors_research/LeBohec.png" alt="Stephan LeBohec" width="438" height="438">
-                                    </a>
-                                    <h3 style="text-align: center;">Stephan LeBohec</h3>
-                                    <p>
-                                        <span>Stephan studies the possible implications of extending relativity to resolution scale
-                                                                            transformations. He also participates in the redevelopment of intensity interferometry
-                                                                            for astrophysical observations. Recently he started working on the mechanical and
-                                                                            thermal noise characterization of mirrors used in gravitational wave detectors.</span>
-                                    </p>
-                                    <ul>
-                                        <li>
-                                            <a href="/gammaray/">Gamma Ray Astronomy/VERITAS</a>
-                                        </li>
-                                        <li>
-                                            <a href="/starbase/">StarBase</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="grid-item col-4">
-                                    <img src="https://www.physics.utah.edu/_images/professors_research/Sandick.1.png" alt="Pearl Sandick" width="585" height="585">
-                                    <br>
-                                    <h3 style="text-align: center;">Pearl Sandick</h3>
-                                    <p>
-                                        <span>Pearl studies high energy phenomenology; particle astrophysics; cosmology. Her specialties
-                                                                            include dark matter, supersymmetry, and other physics beyond the Standard Model.</span>
-                                    </p>
-                                    <ul>
-                                        <li>
-                                            <a href="http://www.physics.utah.edu/~sandick/">Research Website</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="grid-item col-4">
-                                    <p>
-                                        &nbsp;
-                                        <img src="https://www.physics.utah.edu/_images/professors_research/Springer.png" alt="Wayne Springer" width="601" height="601">
-                                    </p>
-                                    <h3 style="text-align: center;">Wayne Springer</h3>
-                                    <p>
-                                        <span>Wayne specializes in developing and using instruments to observe the Cosmos. He has
-                                                                            investigated the Z boson and searched for the Higgs boson at CERN and has studied
-                                                                            ultra-high energy cosmic rays with the HiRes and Telescope Array cosmic ray observatories
-                                                                            in the west desert of Utah. He is currently using the HAWC observatory located on
-                                                                            a Mexican volcano to study TeV gamma and cosmic rays from astrophysical sources and
-                                                                            to search for Dark Matter.</span>
-                                    </p>
-                                    <ul>
-                                        <li>
-                                            <a href="/weo/">Willard L. Eccles Observatory</a>
-                                        </li>
-                                        <li>
-                                            <a href="http://www.hawc-observatory.org/">HAWC</a>
-                                        </li>
-                                        <li>
-                                            <a href="http://web.utah.edu/astro/">South Physics Observatory</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="grid-item col-4">
-                                    &nbsp;
-                                    <a href="http://www.physics.utah.edu/~zhaoyue/">
-                                        <img src="https://www.physics.utah.edu/_images/professors_research/Zhao.png" alt="Yue Zhao" width="438" height="438">
-                                    </a>
-                                    <h3 style="text-align: center;">Yue Zhao</h3>
-                                    <p>
-                                        <span>Yue's research interests are in new physics beyond the standard model. He has been
-                                                                            working on several different methods in order to explore new physics. He is very interested
-                                                                            in developing novel experimental ideas for new physics, including utilizing recent
-                                                                            advances in precision measurement techniques. He is also interested in exploring the
-                                                                            possibility of extending the purposes of existing experiments, such as LIGO and DUNE,
-                                                                            to look for new physics. The new field of gravitational wave astronomy is particularly
-                                                                            exciting to him, and studying new physics through gravitational wave will be one of
-                                                                            his main focuses in the future.</span>
-                                    </p>
-                                    <ul>
-                                        <li>
-                                            <a href="http://www.physics.utah.edu/~zhaoyue/">Research Website</a>
-                                        </li>
-                                    </ul>
                                 </div>
                             </div>
                         </div>
