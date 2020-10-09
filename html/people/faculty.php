db53f9653e3c6b340e7483444e97765c099a5388
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-09 15:44:00 -0600

    test

--- html/people/faculty.php
+++ html/people/faculty.php
@@ -6,17 +6,20 @@
                 "image" => "https://faculty.utah.edu/bytes/image.hml?id=u0095165",
                 "homepage" => "http://www.physics.utah.edu/~bromley",
                 "keywords" => "Planet formation, Solar System Planets, Origins of Milky Way Stars, Milky Way Structure, Astrophysics.",
-                "datascience" => "Applications of Parallel Supercomputing: Dynamical Simulation, Data Mining."),
+                "datascience" => "Applications of Parallel Supercomputing: Dynamical Simulation, Data Mining."
+                ),
             "Joel Brownstein" => array(
                 "image" => "https://faculty.utah.edu/bytes/image.hml?id=u0707758",
                 "homepage" => "https://faculty.utah.edu/~joelbrownstein",
                 "keywords" => "Galaxy Clustering and Evolution, Dark Matter, Gravitational Lensing, Cosmology, Gravity",
-                "datascience" => "Machine Learning, Deep Learning, Data Mining, Data Access, Data Visualization"),
+                "datascience" => "Machine Learning, Deep Learning, Data Mining, Data Access, Data Visualization"
+                ),
             "Tabitha Buehler" => array(
                 "image" => "https://faculty.utah.edu/bytes/image.hml?id=u0707758",
                 "homepage" => "http://faculty.utah.edu/u0791607-TABITHA_CHRISTI_BUEHLER/research/index.hml",
                 "keywords" => "Best Teaching Practices, Public Outreach, Informal Education Engagement",
-                "datascience" => "");
+                "datascience" => ""
+                )
         );
     } catch (Exception $e) {
         echo $e->getMessage();
