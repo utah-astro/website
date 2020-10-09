db07baf7852df680c3a5a66420abd1c575265637
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-09 14:48:45 -0600

    test

--- html/people/functions.php
+++ html/people/functions.php
@@ -6,7 +6,7 @@
                 $image = $member["image"];
                 $homepage = $member["homepage"];
                 $keywords = $member["keywords"];
-                $datascience = $member["$datascience"];
+                $datascience = $member["datascience"];
                 echo "<tr>";
                 echo "    <td width='150'><a href='$homepage'><img src='$image' alt='$name' width='150' height='150'></a></td>";
                 echo "    <td>";
