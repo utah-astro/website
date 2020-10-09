2268d03e8e393f8c7ff02364a2a7bb553d05c5a1
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-09 14:22:17 -0600

    test

--- html/people/functions.php
+++ html/people/functions.php
@@ -7,7 +7,7 @@
                 $homepage = $member["homepage"];
                 $keywords = $member["keywords"];
                 echo "<tr>";
-                echo "    <td width='150'><img src='$image' alt='$name' width='150' height='150'></td>";
+                echo "    <td width='150'><a href='$homepage'><img src='$image' alt='$name' width='150' height='150'></a></td>";
                 echo "    <td>";
                 echo "        <h4><a href='$homepage'>$name</a></h4>";
                 echo "        <span>$keywords</span>";
