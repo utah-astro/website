519f23e7d7e7e8941d99c0010a42fc8fd62b1179
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-09 15:48:08 -0600

    test

--- html/people/functions.php
+++ html/people/functions.php
@@ -5,8 +5,8 @@
             foreach ($members as $name => $member) {
                 $image = $member["image"];
                 $homepage = $member["homepage"];
-                //$keywords = $member["keywords"];
-                //$datascience = $member["datascience"];
+                $keywords = $member["keywords"];
+                $datascience = $member["datascience"];
                 echo "<tr>";
                 echo "    <td width='150'><a href='$homepage'><img src='$image' alt='$name' width='150' height='150'></a></td>";
                 echo "    <td>";
