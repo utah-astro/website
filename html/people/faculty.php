7a710cfea211780bf3efacf3109ac017c5c3bea6
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-09 16:41:48 -0600

    test

--- html/people/faculty.php
+++ html/people/faculty.php
@@ -11,7 +11,7 @@
                 "image" => "https://faculty.utah.edu/bytes/image.hml?id=u0707758",
                 "homepage" => "https://faculty.utah.edu/~joelbrownstein",
                 "keywords" => "Galaxy Clustering and Evolution, Dark Matter, Gravitational Lensing, Cosmology, Gravity, Data Access, Data Visualization, Machine Learning",
-                "affils" => "<a target='_blank' href='https://data.sdss.org'>SDSS Data</a>, <a target='_blank' href='https://test.sdss5.org'>SDSS-V (dev)</a>, <a target='_blank' href='https://datascience.utah.edu'>Utah Center For Data Science</a>"
+                "affiliations" => "<a target='_blank' href='https://data.sdss.org'>SDSS Data</a>, <a target='_blank' href='https://test.sdss5.org'>SDSS-V (coming soon)</a>, <a target='_blank' href='https://datascience.utah.edu'>Utah Center For Data Science</a>"
                 ),
             "Tabitha Buehler" => array(
                 "image" => "https://faculty.utah.edu/bytes/image.hml?id=u0791607",
@@ -22,7 +22,7 @@
                 "image" => "https://faculty.utah.edu/bytes/image.hml?id=u0634757",
                 "homepage" => "http://faculty.utah.edu/u0634757-KYLE_DAWSON/research/index.hml",
                 "keywords" => "Origins of Cosmic Acceleration, Cosmology, Galaxy Clustering"
-                "affils" => "<a target='_blank' href='https://www.sdss.org/surveys/eboss/'>eBOSS</a>, <a target='_blank' href='https://www.desi.lbl.gov'>DESI</a>, <a target='_blank' href='https://datascience.utah.edu'>Utah Center For Data Science</a>"
+                "affiliations" => "<a target='_blank' href='https://www.sdss.org/surveys/eboss/'>eBOSS</a>, <a target='_blank' href='https://www.desi.lbl.gov'>DESI</a>, <a target='_blank' href='https://datascience.utah.edu'>Utah Center For Data Science</a>"
                 )
         );
     } catch (Exception $e) {
--- html/people/functions.php
+++ html/people/functions.php
@@ -2,19 +2,19 @@
     function membertable($members) {
         if ( isset($members) ) {
             $astro = fa('star-o');
-            $conn = fa('connectdevelop');
+            $university = fa('university');
             echo "<table class='table table-header-shade table-bordered'>";
             foreach ($members as $name => $member) {
                 $image = $member["image"];
                 $homepage = $member["homepage"];
                 $keywords = $member["keywords"];
-                $dataconn = $member["dataconn"];
+                $affiliations = $member["affiliations"];
                 echo "<tr>";
                 echo "<td width='150'><a href='$homepage'><img src='$image' alt='$name' width='150' height='150'></a></td>";
                 echo "<td>";
                 echo "<h4><a href='$homepage'>$name</a></h4>";
                 if ( isset($keywords) ) {echo "<p>$astro $keywords</p>";}
-                if ( isset($dataconn) ) {echo "<p>$conn $dataconn</p>";}
+                if ( isset($affiliations) ) {echo "<p>$university $affiliations</p>";}
                 echo "</td>";
                 echo "</tr>";
             }
