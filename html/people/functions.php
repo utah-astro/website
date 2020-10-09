675663273a407d1db0ea21d93eb375983e244f49
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-09 14:19:13 -0600

    test

--- html/people/functions.php
+++ html/people/functions.php
@@ -4,10 +4,10 @@
             echo "<table class='table table-header-shade table-bordered'>";
             foreach ($members as $name => $member) {
                 echo "<tr>";
-                echo "    <td width='150'><img src='$member->image' alt='$name' width='150' height='150'></td>";
+                echo "    <td width='150'><img src=\"$member['homepage']\" alt='$name' width='150' height='150'></td>";
                 echo "    <td>";
-                echo "        <h4><a href='$member->homepage'>$name</a></h4>";
-                echo "        <span>$member->keywords</span>";
+                echo "        <h4><a href=\"$member['homepage']\">$name</a></h4>";
+                echo "        <span>$member['keywords']</span>";
                 echo "    </td>";
                 echo "</tr>";
             }
