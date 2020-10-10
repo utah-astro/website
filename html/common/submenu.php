aa97c0b692d4b9cf67b1b5c8a3f7a9f254456f9c
Joel Brownstein <joelbrownstein@sdss.org>
2020-10-10 15:39:38 -0600

    submenu emphasis

--- html/common/submenu.php
+++ html/common/submenu.php
@@ -5,7 +5,7 @@
                 <div class="skip-subnav">
                     <a href="#skip-subnav-target">Skip Menu</a>
                 </div>
-                <h3 style="color: #666;">Astro Group</h3>
+                <h3 style="color: #966;">Astro Group</h3>
                 <ul>
                     <li class="">
                         <a href="../overview/">Overview</a>
@@ -32,7 +32,7 @@
                         <a target="_blank" href="https://observatory.astro.utah.edu/">South Physics Observatory (on campus)</a>
                     </li>
                 </ul>
-                <h4 style="color: #666;">Events</h4>
+                <h4 style="color: #966;">Events</h4>
                 <ul>
                     <li class="">
                         <a target="_blank" href="https://www.physics.utah.edu/~heap">HEAP Seminar</a>
