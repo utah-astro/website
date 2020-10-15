6eee2f4716412b99ce2853f90990c6c0658cb272
GailZasowski <gail.zasowski@gmail.com>
2020-10-15 15:49:05 -0600

    Update postdocs.php
    added all postdocs

--- html/people/postdocs.php
+++ html/people/postdocs.php
@@ -2,20 +2,40 @@
 <?php
     try {
         $members = array(
-            "Nicholas Boardman" => array(
+            "Angela Berti" => array(
                 "image" => "https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg",
+                "keywords" => "",
+                "homepage" => ""
+                ),
+            "Nicholas Boardman" => array(
+                "image" => "https://lh4.googleusercontent.com/F5x87f7w36e0B80HsCO54OJSWWIDGk6sacFZEbPmnZ5qFDsXPOEjaYD7YzCMgFaB9_19KV1O=w1280",
                 "keywords" => "Galaxy structure and evolution, Milky Way Analogs",
                 "homepage" => "https://sites.google.com/view/nickybastro/home"
                 ),
-            "Sten Hasselquist" => array(
+            "Sarah Eftekharzadeh" => array(
                 "image" => "https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg",
-                "keywords" => "Galaxy evolution, dwarf galaxies, stellar spectroscopy and chemcial abundances",
+                "keywords" => "",
+                "homepage" => ""
+                ),
+            "Sten Hasselquist" => array(
+                "image" => "https://drstenhq.com/wp-content/uploads/2018/12/IMG_8270-768x632.jpg",
+                "keywords" => "Galaxy evolution, dwarf galaxies, stellar spectroscopy and chemical abundances",
                 "homepage" => "https://drstenhq.com/"
                 ),
             "Jianhui Lian" => array(
+                "image" => "https://static.wixstatic.com/media/70a18c_d0f6465072e34e569f4b66de4abc5251~mv2.jpg/v1/fill/w_546,h_697,al_c,q_85,usm_0.66_1.00_0.01/70a18c_d0f6465072e34e569f4b66de4abc5251~mv2.webp",
+                "keywords" => "Chemical evolution and star formation histories of galaxies, galaxy environment and structure",
+                "homepage" => "https://ljh520hw.wixsite.com/mysite"
+                ),
+            "Aysegul Tumer" => array(
                 "image" => "https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg",
                 "keywords" => "",
-                "homepage" => "https://ljh520hw.wixsite.com/mysite"
+                "homepage" => ""
+                ),
+            "Qian Wang" => array(
+                "image" => "https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg",
+                "keywords" => "",
+                "homepage" => ""
                 ),
         );
     } catch (Exception $e) {
