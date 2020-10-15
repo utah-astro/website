1b618c5d1df70d2a0cae2473f1d379f4723422f8
GailZasowski <gail.zasowski@gmail.com>
2020-10-15 13:01:20 -0600

    Update postdocs.php
    added some postdocs

--- html/people/postdocs.php
+++ html/people/postdocs.php
@@ -2,11 +2,21 @@
 <?php
     try {
         $members = array(
-            "Name Here" => array(
+            "Nicholas Boardman" => array(
                 "image" => "https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg",
-                "keywords" => "Research topics",
-                "homepage" => ""
-                )
+                "keywords" => "Galaxy structure and evolution, Milky Way Analogs",
+                "homepage" => "https://sites.google.com/view/nickybastro/home"
+                ),
+            "Sten Hasselquist" => array(
+                "image" => "https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg",
+                "keywords" => "Galaxy evolution, dwarf galaxies, stellar spectroscopy and chemcial abundances",
+                "homepage" => "https://drstenhq.com/"
+                ),
+            "Jianhui Lian" => array(
+                "image" => "https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg",
+                "keywords" => "",
+                "homepage" => "https://ljh520hw.wixsite.com/mysite"
+                ),
         );
     } catch (Exception $e) {
         echo $e->getMessage();
