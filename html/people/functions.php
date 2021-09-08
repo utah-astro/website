3f29d678531c4955360f0fd7779235e3bc30b13f
Joel Brownstein <joelbrownstein@astro.utah.edu>
2021-09-08 16:07:52 -0600

    add phd_thesis, date, contact for allumni page

--- html/people/functions.php
+++ html/people/functions.php
@@ -4,6 +4,9 @@
             $astro = fa('star-o');
             $link = fa('external-link');
             $university = fa('university');
+            $book = fa('book');
+            $address = fa('address-book');
+            $calendar = fa('calendar-o');
             echo "<table class='table table-header-shade table-bordered'>";
             foreach ($members as $name => $member) {
                 $image = $member["image"];
@@ -11,13 +14,19 @@
                 $keywords = $member["keywords"];
                 $links = $member["links"];
                 $advisor = $member["advisor"];
+                $phd_thesis = $member["phd_thesis"];
+                $date = $member["phd_date"];
+                $contact = $member["contact"];
                 echo "<tr>";
                 echo "<td width='150'><a target='_homepage' href='$homepage'><img src='$image' alt='$name' width='150' height='150'></a></td>";
                 echo "<td>";
                 echo "<h4><a target='_homepage' href='$homepage'>$name</a></h4>";
                 if ( ! empty($keywords) ) {echo "<p>$astro $keywords</p>";}
                 if ( ! empty($links) ) {echo "<p>$link $links</p>";}
+                if ( ! empty($phd_thesis) ) {echo "<p>$book Ph.D. Thesis: $phd_thesis</p>";}
                 if ( ! empty($advisor) ) {echo "<p>$university Advisor: $advisor</p>";}
+                if ( ! empty($date) ) {echo "<p>$calendar $date</p>";}
+                if ( ! empty($contact) ) {echo "<p>$address Current Position: $contact</p>";}
                 echo "</td>";
                 echo "</tr>";
             }
