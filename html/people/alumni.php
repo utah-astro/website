09957ac9e834a91ac7a5da75bd65d68c1af0c164
GailZasowski <gail.zasowski@gmail.com>
2021-10-05 10:51:36 -0600

    add Jianhui & Vijith, sort by date of departure

--- html/people/alumni.php
+++ html/people/alumni.php
@@ -2,26 +2,36 @@
 <?php
     try {
         $members = array(
-            "Hong Guo" => array(
-                "image" => "http://sdss4.shao.ac.cn/guoh/img/about.jpg",
-                "homepage" => "http://sdss4.shao.ac.cn/guoh",
-                "date" => "Postdoc at the U until Aug. 2015",
-                "position" => "Research Professor at Shanghai Astronomical Observatory, Chinese Academy of Sciences",
-                ),
-            "Sarah Eftekharzadeh" => array(
+            "Michael S. Talbot" => array(
                 "image" => "https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg",
-                "date" => "Postdoc at the U until Dec. 2020",
+                "advisor" => "Prof. Joel Brownstein",
+                "phd_thesis" => "Extending the Search for Strong Galaxy-Galaxy Gravitational Lenses within SDSS",
+                "date" => "Ph.D. 2021",
+                ),
+            "Vijith Jacob Poovelil" => array(
+                "image" => "../resources/jpg/vijith_poovelil.jpeg",
+                "homepage" => "https://faculty.utah.edu/u1063369-VIJITH_JACOB_POOVELIL/research/index.hml", 
+                "advisor" => "Prof. Gail Zasowski",
+                "phd_thesis" => "Exploring Galactic Chemical Evolution Using Cluster Chemistry and Variability",
+                "date" => "Ph.D. 2021",
+                "position" => "",
                 ),
             "Nicholas Boardman" => array(
                 "image" => "../resources/jpg/nick_boardman.jpg",
                 "homepage" => "https://sites.google.com/view/nickybastro/home",
                 "date" => "Postdoc at the U until Aug. 2021",
-                "position" => "Postdoc at University of St Andrews",
+                "position" => "Postdoc at the University of St Andrews",
+                ),
+            "Jianhui Lian" => array(
+                "image" => "../resources/jpg/jianhui_lian.jpg",
+                "homepage" => "https://ljh520hw.wixsite.com/mysite"
+                "date" => "Posdoc at the U until Aug. 2021",
+                "position" => "Postdoc at Max-Planck-Institut f&uuml;r Astronomie",
                 ),
             "Sten Hasselquist" => array(
                 "image" => "https://drstenhq.com/wp-content/uploads/2018/12/IMG_8270-768x632.jpg",
                 "homepage" => "https://drstenhq.com/",
-                "date" => "Postdoc the U until Aug. 2021",
+                "date" => "Postdoc at the U until June 2021",
                 "position" => "Staff scientist at Space Telescope Science Institute",
                 ),
             "Kevin S. McCarthy" => array(
@@ -31,11 +41,15 @@
                 "date" => "Ph.D. 2021",
                 "position" => "NASA Postdoc at IPAC/JPL",
                 ),
-            "Michael S. Talbot" => array(
+            "Sarah Eftekharzadeh" => array(
                 "image" => "https://hcav.am/wp-content/uploads/2020/03/work-on-laptop-icon-150x150.jpg",
-                "advisor" => "Prof. Joel Brownstein",
-                "phd_thesis" => "Extending the Search for Strong Galaxy-Galaxy Gravitational Lenses within SDSS",
-                "date" => "Ph.D. 2021",
+                "date" => "Postdoc at the U until Dec. 2020",
+                ),
+            "Hong Guo" => array(
+                "image" => "http://sdss4.shao.ac.cn/guoh/img/about.jpg",
+                "homepage" => "http://sdss4.shao.ac.cn/guoh",
+                "date" => "Postdoc at the U until Aug. 2015",
+                "position" => "Research Professor at Shanghai Astronomical Observatory, Chinese Academy of Sciences",
                 ),
         );
     } catch (Exception $e) {
