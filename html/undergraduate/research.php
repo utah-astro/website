8b4bdaa5ba154921c2f88325b58fe4e580160064
danielrwik <danielrwik@gmail.com>
2021-11-17 02:34:44 -0700

    Starter content added to research.php

--- html/undergraduate/research.php
+++ html/undergraduate/research.php
@@ -26,19 +26,133 @@
                         <div class="uu-main-content">
                             <p><img class="img-float-left" src="https://lmg-labmanager.s3.amazonaws.com/assets/articleNo/22987/aImg/42595/mad-scientist-stereotype-m.webp"></p>
                             <p>
-                                So, you want to get your hands dirty with some astronomy research?
-                                Well, you've come to the right place!
-                                This page will serve as the jumping off point of your hero's journey to fame, riches, and more!
+                                Welcome to the world (worlds? galaxies?) of astronomy research!
+                                This page is meant to inform prospective undergraduate researchers about what skills are generally
+                                most useful for research in our group, how to acquire those skills,
+                                and how to go about finding a group/project to work on.
+                            </p>
+                            <h3>Astronomy Research Overview and Useful Skills</h3>
+                            <p>
+                                Getting involved in astronomial research can be valuable for a variety of reasons, the most
+                                common of which is in preparation for graduate school (or deciding whether grad school is for you). 
+                                Other reasons include securing experience on a resume 
+                                for a private sector job with a research or problem solving focus,
+                                completing an Honors Thesis, and
+                                simply finding astronomy to be, like, super cool.
                             </p>
                             <p>
+                                Modern astronomical research is fundamentally computational in nature,
+                                with trips to physical telescopes on mountaintops (observing runs) for observational
+                                astronomers becoming rare and with analytical (pen and paper) investigations for solving
+                                theoretical problems in astronomy becoming more intractable.
+                                Both observers and theorists today make significant use of computer processing to analyze
+                                data and perform calculations, as data (whether measured or simulated) is now almost always digital.
+                                While not always required, at least some prior programming ability/exposure is a plus when
+                                considering whether to take on an undergraduate researcher.
+                                The most common/versatile programming language in use today is python, and the ability to work
+                                in a command line or terminal environment within a unix-based operating system is particularly useful.
+                            </p>
+                            <p>
+                                As an undergraduate still taking classes in astronomy (if you have taken any yet!), detailed background
+                                knowledge of the scientific problem at hand in a research project is not needed or expected---you will
+                                learn this information as part of the research experience.
+                                Scientific research as a profession trains new practitioners via the apprentice system;
+                                research is learned by doing under the supervision of a mentor or mentors, with the first research
+                                experience including significant direction and step-by-step instruction.
+                                However, mentor time is always at a premium, so research success depends strongly on the student's
+                                ability to work independently, finding solutions to problems on their own (in conjunction with all
+                                resources at their disposal, including the expertise of other group members), and taking ownership of
+                                their project.
+                            </p>
+                            <p>
+                                While you or your mentor may consider a first project as more of an unpaid internship, with you volunteering
+                                your time to learn the ropes, whenever possible we like our researchers to be paid.
+                                There are multiple programs at the U to fund undergraduate research, such as UROP, SURP, and a research
+                                group's external funding.
+                                When meeting with potential mentors, be sure to inquire about how to get paid---sometimes there is already
+                                a source of funding available, but other times an application to ask for funding (from, e.g., UROP or SURP)
+                                is necessary.
+                                Especially if being paid is crucial---and not getting paid means not doing research, because there are bills
+                                to pay---let mentors know so they can work with you.
+                            </p>
+                            <h3>When to Start Doing Research</h3>
+                            <p>
+                                In short, as soon as you can!
+                                Depending on your background, you could start right away, during your first year at the U, or it may
+                                take you until your final summer before graduation to work on a project.
+                                While it's never too late to get involved in principle, there's no guarantee an appropriate project will
+                                be available when you need it, so the sooner you can find a group and project, then better.
+                            </p>
+                            <p>
+                                Over the past couple decades, graduate school has become more competitive, with undergraduate 
+                                research experience now a de facto requirement for admission.
+                                Multiple research experiences, especially at other institutions through Research Experience for Undergraduate
+                                (REU) programs, are especially helpful (but not necessary).
+                                Below, we lay out an optimistic timeline for someone who comes to the U planning on going to grad school
+                                in astronomy.
+                            </p>
                                 <ul>
-                                    <li> This bit will tell folks what research is good for.</li>
+                                    <li>
+                                        <b>First Year:</b> 
+                                        Learn python or another programming language.
+                                        Learn about the various faculty-led research in the Astro Group and talk with students currently doing
+                                        research in these groups about their work.
+                                        This information is often not readily available---the department Discord thingy can be a great way
+                                        to find out which students are working with whom.
+                                        Also, find out how the undergraduate students got involved in their group in addition to how they like
+                                        their project and what skills they need to carry it out.
+                                        While this webpage is meant to help you learn the ropes, it can't provide the full story.
+                                        If a particular research area interests you, contact that faculty member asking to learn more about
+                                        their research (faculty at various times for various reasons will be unable to take on new students,
+                                        but in general they are happy to talk about their research).
+                                    </li>
 
-                                    <li> This bit will lay out timelines (when to do research).</li>
+                                    <li> 
+                                        <b>Second Year:</b>
+                                        Now's when you want to find a mentor and project.
+                                        If you have time during the fall or spring semesters, you can get started on a project
+                                        as soon as you find a mentor who has one.
+                                        Then, if feasible, secure a research position to work on that project over the summer,
+                                        either full or part time.
+                                        Over the summer with its reduced distractions, 
+                                        it's easier to focus on a project and make good progress.
+                                        In addition, become more involved in the activities of the Astro group.
+                                        Our main weekly activities are attending talks at the HEAP seminar series and
+                                        discussing the most recent astronomy research papers at Astro-Coffee.
+                                        We now regularly communicate via a Slack channel as well, where these and other events
+                                        are advertised---the channel includes faculty, postdocs, and graduate and undergraduate
+                                        students---ask for an invitation!
+                                        Because at this point you may only have taken ASTR 3070, if that, these talks and discussions
+                                        will often go over your head, which is totally normal.
+                                        The purpose of attending is not to understand everything, but to start to pick up jargon
+                                        and get to know us---there is no need to directly participate, although questions are
+                                        always welcome.
+                                    </li>
 
-                                    <li> This bit will explain what background is needed to effectively do research.</li>
+                                    <li> 
+                                        <b>Third Year:</b>
+                                        While your focus will mostly be on classes, you will likely also continue working
+                                        on your research project, potentially presenting your work at a local or professional
+                                        conference.
+                                        Now is the time to apply for REU programs, which take place over the following summer,
+                                        to work on a new project at a different institution.
+                                        These programs are now competitive, so having past research experience of some kind
+                                        is needed to be accepted into them.
+                                        Having a second research experience outside your college will provide another perspective
+                                        (and recommendation letter) on your promise as a graduate student to grad programs.
+                                        While attending an REU is not necessary, it is generally a great experience and can
+                                        open the doors to more competitve graduate programs.
+                                    </li>
                                   
-                                    <li> Profit! </li>
+                                    <li>
+                                        <b>Fourth Year:</b>
+                                        Make sure you graduate and, if possible, write up the results of one of your research
+                                        projects in a scientific paper.
+                                        Or similarly, write up your work in an Honors Thesis.
+                                        Also, obviously, now is when you apply to grad school (end of fall / start of spring semesters).
+                                        At this point, you'll know the ropes and can help first and second year students
+                                        find their way.
+                                    </li>
                                 </ul>
                             </p>
                         
