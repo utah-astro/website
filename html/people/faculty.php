69a0bbb9253e27fc7f1fc21134ec4b09d07d4509
Joel Brownstein <joelbrownstein@astro.utah.edu>
2025-07-12 21:31:07 -0600

    fix broken link to www-physics

--- html/people/faculty.php
+++ html/people/faculty.php
@@ -10,7 +10,7 @@
                 ),
             "Joel Brownstein" => array(
                 "image" => "../resources/jpg/joelbrownstein.jpg",
-                "homepage" => "https://www.physics.utah.edu/~joelbrownstein",
+                "homepage" => "https://astro.utah.edu/people/joelbrownstein",
                 "keywords" => "Galaxy Clustering and Evolution, Dark Matter, Gravitational Lensing, Cosmology, Gravity, Data Access, Data Visualization, Machine Learning",
                 "links" => "<a target='_blank' href='https://data.sdss.org'>SDSS Data</a>, <a target='_blank' href='https://www.sdss.org'>SDSS-V</a>, <a target='_blank' href='https://datascience.utah.edu'>Utah Center For Data Science</a>, <a target='_blank' href='http://admin.masterlens.org/index.php'>The Master Lens Database</a>, <a target='_blank' href='https://scholar.google.com/citations?user=Yr5IIFEAAAAJ'>Google Scholar Citations</a>, <a target='_blank' href='https://orcid.org/0000-0002-8725-1069'>ORCiD Works</a>",
                 "office" => "INSCC 312",
